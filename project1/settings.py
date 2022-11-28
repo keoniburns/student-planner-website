7582cc715004a75dd267e339f455733869f626b3
keoni Burns <keoni.burns@gmail.com>
2022-11-28 14:45:06 -0800

    added the calendar app to settings

--- project1/settings.py
+++ project1/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'budget',
     'core',
     'tasks',
-    'LFG'
+    'LFG',
+    'calendar'
 ]
 
 MIDDLEWARE = [
@@ -123,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [ STATIC_DIR ]
+STATICFILES_DIRS = [STATIC_DIR]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
